4646d17b93b74420ffbeedcf5da5c7e95e74cb5c
Gubina Irina <irina.gubina98@mail.ru>
2021-07-02 17:03:08 +0300

    Удален лишний метод

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,8 +1,6 @@
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
-import org.testng.annotations.*;
-
 
 
 @CucumberOptions(
@@ -12,8 +10,4 @@ import org.testng.annotations.*;
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-    @Test
-    public static void runTest() {
-
-    }
 }
